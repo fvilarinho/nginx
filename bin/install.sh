1273cf63d0e6f0c2d024ee48900d2f3762625003
Felipe Vilarinho <fvilarinho@innovativethinking.com.br>
2021-03-03 20:00:49 -0300

    Bug fixes.

--- bin/install.sh
+++ bin/install.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+cp $ETC_DIR/nginx/conf.d/default.conf /tmp/default.conf
+sed -i -e 's|${PORT}|'"$PORT"'|g' /tmp/default.conf
+sed -i -e 's|${SECURE_PORT}|'"$SECURE_PORT"'|g' /tmp/default.conf
+cp -f /tmp/default.conf /etc/nginx/conf.d/default.conf
--- bin/startup.sh
+++ bin/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SECURE_PORT=`echo $SETTINGS | jq -r .securePort.value`
+
+export SECURE_PORT
+
 $BIN_DIR/child-install.sh
 
 nginx
